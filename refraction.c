651a75da1fd9168c5fe3e6961c8813b7ede653cb
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-05 17:34:15 -0500

    Create refraction.c

---
+++ refraction.c
@@ -0,0 +1,10 @@
+/**Author: SungHa Park
+ *
+ *This program computes angle of refraction
+ */
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv) {
+  printf("Enter the value of Angle of Incidence")
