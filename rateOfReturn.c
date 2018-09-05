05de662fc7a0e1cd8e368f464ee174c2d6a51af2
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-05 18:09:39 -0500

    Create rateOfReturn.c

---
+++ rateOfReturn.c
@@ -0,0 +1,20 @@
+/**
+ *Author: SungHa Park
+ *This program computes and outputs the annualized rate of return
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv){
+  
+  double InitialValue, FinalValue
+  
+  printf("Enter the initial value of investments: ");
+  scanf("%lf", &InitialValue);
+  
+  printf("Enter the final value of investments: ");
+  scanf("%lf", &FinalValue);
+  
+  
