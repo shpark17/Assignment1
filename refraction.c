b9a310e8a942d9b21fb980f6c755b547cad2afbf
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-05 17:55:11 -0500

    Update refraction.c

--- refraction.c
+++ refraction.c
@@ -7,4 +7,18 @@
 #include<math.h>
 
 int main(int argc, char **argv) {
-  printf("Enter the value of Angle of Incidence")
+  
+  double AngleOfIncidence, RefractionOfTheMedium
+  
+  printf("Enter the value of Angle of Incidence: ");
+  scanf("%lf", &AngleOfIncidence);
+  
+  printf("Enter the Value of index of refraction of the medium: ");
+  scanf("%lf", &RefractionOfTheMedium);
+  
+  double AngleOfRefraction
+  AngleOfRefraction = asin(RefractionOfTheMedium*sin(AngleOfIncidence))
+    
+  printf("Angle of incidence: %f, %f\n Index of Refraction: %f, %f\n Angle of refraction: %f,\n", AngleOfIncidence, RefractionOfTheMedium, AngleOfRefraction);
+return 0;
+}
