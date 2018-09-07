e5a65fbbec73ab5189fe9b928e8bc99107f41544
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-07 15:10:07 -0500

    Update rateOfReturn.c

--- rateOfReturn.c
+++ rateOfReturn.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char **argv){
   
-  double InitialValue, FinalValue, Time, Months, Weeks, Days;
+  double InitialValue, FinalValue, Time, Years, Months, Weeks, Days;
   
   printf("Enter the initial value of investments: ");
   scanf("%lf", &InitialValue);
@@ -18,7 +18,7 @@ int main(int argc, char **argv){
   scanf("%lf", &FinalValue);
   
   printf("Enter the years: ");
-  scanf("%lf", &Time);
+  scanf("%lf", &Years);
   
   printf("Enter the months: ");
   scanf("%lf", &Months);
@@ -29,7 +29,7 @@ int main(int argc, char **argv){
   printf("Enter the days: ");
   scanf("%lf", &Days);
   
-  Time = Time + (Months/12) + (Weeks/52.1429) + (Days/365);
+  Time = Years + (Months/12) + (Weeks/52.1429) + (Days/365);
   double AnnualizedRateOfReturn = (pow((FinalValue/InitialValue), (1/Time)) - 1)*100;
     
   printf("Initial Value: %f\n Final Value: %f\n Years: %f\n Annaulized rate of return: %f%\n", InitialValue, FinalValue, Time, AnnualizedRateOfReturn);
