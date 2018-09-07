41806b45cfd97a3c12b47100a781826ed574e6c7
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-06 21:02:45 -0500

    Update rateOfReturn.c

--- rateOfReturn.c
+++ rateOfReturn.c
@@ -9,12 +9,21 @@
 
 int main(int argc, char **argv){
   
-  double InitialValue, FinalValue
+  double InitialValue, FinalValue, Time
   
   printf("Enter the initial value of investments: ");
   scanf("%lf", &InitialValue);
   
   printf("Enter the final value of investments: ");
   scanf("%lf", &FinalValue);
+  
+  printf("Enter the years: ");
+  scanf("%lf", &Time);
+  
+  double AnnualizedRateOfReturn = pow((FinalValue/InitialValue), (1/Time))
+    
+  printf("Initial Value: %f\n Final Value: %f\n Years: %f\n Annaulized rate of return: %f\n", InitialValue, FinalValue, TIme, AnnualizedRateOfReturn);
+return 0;
+}  
   
   
